0d5a910ea94db9c3dcee53439616564431838d00
yds <ydeltastar@gmail.com>
2024-11-13 02:22:02 -0300

    Render meshes in custom render mode

--- addons/scene_texture/SceneRenderCustom.gd
+++ addons/scene_texture/SceneRenderCustom.gd
@@ -7,19 +7,36 @@ extends SceneRender
 
 
 # --- Public Functions --- #
-func render(iteration: int):
-	#RenderingServer.call_on_render_thread(_render_subviewport.bind(self, iteration))
-	#await render_finished
-	#return _texture.get_image()
+func render(iteration: int):	
+	await get_tree().process_frame
 	
-	const ICON = preload("res://icon.svg")
-	var image = ICON.get_image()
-	image.resize(size.x, size.y)
+	var mesh_instances = _get_children_of_type(self, MeshInstance3D)
+	print(mesh_instances)
+	var meshes: Array[Mesh]
+	for instance: MeshInstance3D in mesh_instances:
+		if instance.mesh:
+			meshes.append(instance.mesh)
+		instance.get_instance()
 	
-	_render = image
-	#var new_texture = RenderingServer.texture_2d_create(image)
-	#RenderingServer.texture_replace(get_texture(), new_texture)
+	if Engine.is_editor_hint() and meshes.size() > 0:
+		var textures = EditorInterface.make_mesh_previews(meshes, size.x)
+		_render = textures[0].get_image()
+	else:
+		var p = Image.create_empty(size.x, size.y, true, Image.FORMAT_RGB8)
+		p.fill(Color.DEEP_PINK)
+		_render = p
+	
 	render_finished.emit()
 
 
 # --- Private Functions --- #
+static func _get_children_of_type(node:Node, type, recursive = true) -> Array[Node]:
+	var desired_children:Array[Node] = []
+	for child in node.get_children():
+		if is_instance_of(child, type):
+			desired_children.append(child)
+
+		if recursive:
+			desired_children.append_array(_get_children_of_type(child, type, recursive))
+
+	return desired_children
