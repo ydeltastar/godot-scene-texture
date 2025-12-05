80a12b8fd68496f5d2966f26b45d343878c4ddf7
ydeltastar <ydeltastar@gmail.com>
2025-12-05 20:02:31 -0300

    Ensure SceneRenderManager autoload is added when plugin is actived

--- addons/scene_texture/editor/SceneTexturePlugin.gd
+++ addons/scene_texture/editor/SceneTexturePlugin.gd
@@ -5,30 +5,25 @@ extends EditorPlugin
 var _scene_texture_inspector = preload("res://addons/scene_texture/editor/SceneTextureInspector.gd").new()
 
 
-func _enable_plugin() -> void:
-	add_autoload_singleton("SceneRenderManager", get_script().resource_path.get_base_dir() + "/../SceneRenderManager.gd")
-	
-
-func _disable_plugin() -> void:
-	remove_autoload_singleton("SceneRenderManager")
-	
-
 func _enter_tree() -> void:
 	_add_setting("scene_texture/auto_bake_delay", 0.01)
 	_add_setting("scene_texture/default_world_3d", "", TYPE_STRING, PROPERTY_HINT_FILE)
-	
+
+	if not Engine.has_singleton("SceneRenderManager"):
+		add_autoload_singleton("SceneRenderManager", get_script().resource_path.get_base_dir() + "/../SceneRenderManager.gd")
 	add_inspector_plugin(_scene_texture_inspector)
 
 
 func _exit_tree() -> void:
 	remove_inspector_plugin(_scene_texture_inspector)
+	remove_autoload_singleton("SceneRenderManager")
 
 
 func _add_setting(property_name: String, default: Variant, type = -1, hint = -1, hint_string = ""):
 	if not ProjectSettings.has_setting(property_name):
 		ProjectSettings.set_setting(property_name, default)
 	ProjectSettings.set_initial_value(property_name, default)
-	
+
 	if type != -1:
 		var property_info = {
 			"name": property_name,
@@ -37,5 +32,5 @@ func _add_setting(property_name: String, default: Variant, type = -1, hint = -1,
 		if hint != -1:
 			property_info["hint"] = hint
 			property_info["hint_string"] = hint_string
-		
+
 		ProjectSettings.add_property_info(property_info)
