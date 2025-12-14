930dfc673cabc8235012b51f46ad1940dbc5b75b
ydeltastar <ydeltastar@gmail.com>
2025-12-14 10:12:28 -0300

    Remove unused setting

--- addons/scene_texture/editor/SceneTexturePlugin.gd
+++ addons/scene_texture/editor/SceneTexturePlugin.gd
@@ -6,7 +6,6 @@ var _scene_texture_inspector = preload("res://addons/scene_texture/editor/SceneT
 
 
 func _enter_tree() -> void:
-	_add_setting("scene_texture/auto_bake_delay", 0.01)
 	_add_setting("scene_texture/default_world_3d", "", TYPE_STRING, PROPERTY_HINT_FILE)
 
 	if not Engine.has_singleton("SceneRenderManager"):
